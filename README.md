30bd7425380f1639c2fee588e8cd3119ca670c9c
teikitapp <teikit.app@gmail.com>
2025-02-04 17:39:53 -0300

    subida de archivos

--- README.md
+++
@@ -1,1 +0,0 @@
-# Aplicacion web de Teikit
--- teikit-backend-master/src/main/java/com/cliente_usuario/gestion_teikit/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cliente_usuario.gestion_teikit.repository;
-
-import com.cliente_usuario.gestion_teikit.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
